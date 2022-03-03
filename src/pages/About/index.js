175594936ad84827fef64711a6ae6d5e9b6393cd
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2022-03-03 12:54:20 -0600

    added folders to images for Illustrations, Merch, storyboards, and comics for later use

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -9,9 +9,9 @@ const About = () => {
 
                 I was born and raised in Texas until college where I attended the Savannah College of Art and Design in Atlanta, Georgia, graduating in 2017.  I am currently residing in Austin, TX, though who knows where the future may take me!
 
-                I love to read and draw comics and I have a strong passion for video games.  Stories with romance, friendship and a little horror are my absolute favorites.
+                I love to read and draw comics and I have a strong passion for video games. Stories with romance, friendship, and a little horror are my absolute favorites.
 
-                Check out my upcoming convention schedule below or reach out to me through my contact page.
+                Check out my upcoming convention schedule or reach out to me through my contact page!
             </p>
         </div>
     );
